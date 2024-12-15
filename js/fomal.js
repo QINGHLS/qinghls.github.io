3e86c3d4410b20f62bb1984cae0d4bc5fe3eaf13
QINGHLS <sichenemail@163.com>
2024-12-15 21:56:10 +0800

    Site updated: 2024-12-15 21:56:10

--- js/fomal.js
+++ js/fomal.js
@@ -3170,6 +3170,9 @@ let unsplash = "url(https://source.unsplash.com/random/1920x1080/)";
 
 
 // 更换背景(自己的代码)
+/* --mlight-bk: url(https://tuchuang.voooe.cn/images/2024/12/12/mlight.jpg)
+--mnight-bk: url(https://tuchuang.voooe.cn/images/2024/12/12/mnight.jpg) */
+
 if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
@@ -3180,8 +3183,8 @@ if (localStorage.getItem("blogbg") != undefined) {
     --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
     --PClight-bk: url(https://www.blogchen.us.kg/images/bk-50.png);
     --PCnight-bk: url(https://www.blogchen.us.kg/images/720-N.png);
-    --mlight-bk: url(https://tuchuang.voooe.cn/images/2024/12/12/mlight.jpg)
-    --mnight-bk: url(https://tuchuang.voooe.cn/images/2024/12/12/mnight.jpg)
+    --mlight-bk: url(https://www.blogchen.us.kg/images/mlight.jpg)
+    --mnight-bk: url(https://www.blogchen.us.kg/images/mnight.jpg)
   }`;
 }
 // 切换背景主函数
